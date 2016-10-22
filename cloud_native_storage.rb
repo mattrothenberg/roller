cc2cc8c1a0c90d68491a276edcd78a51645c8904
Matt and Mark and David <mrothenberg+markanddavid@gmail.com>
2016-10-22 19:25:00 -0400

    DRY up region
    SOLID

--- cloud_native_storage.rb
+++ cloud_native_storage.rb
@@ -2,11 +2,12 @@ require 'aws-sdk'
 require 'securerandom'
 
 def upload(file_path)
-  s3 = Aws::S3::Resource.new(region:'us-west-2')
+  region = 'us-west-2'
+  s3 = Aws::S3::Resource.new(region: region)
 
   uuid = SecureRandom.uuid
   obj = s3.bucket('objectobjectgifs').object(uuid)
   obj.upload_file(file_path)
 
-  "https://s3-us-west-2.amazonaws.com/objectobjectgifs/#{uuid}"
+  "https://s3-#{region}.amazonaws.com/objectobjectgifs/#{uuid}"
 end
