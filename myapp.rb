a3c7438841d59381deb4cea9d8b3f051a5a42ea9
Matt and Mark and David <mrothenberg+markanddavid@gmail.com>
2016-10-22 15:12:44 -0400

    Add stubby frontend

--- myapp.rb
+++ myapp.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  "Hello World!"
+  File.open('./frontend/index.html') do |file|
+    file.read
+  end
+end
+
+get '/stub' do
+  "Your URL is #{params[:url]} starting from #{params[:start]} to #{params[:end]}"
 end
