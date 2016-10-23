faf8ab455384632c58199d51fe6963f005f0f105
Matt and Mark and David <mrothenberg+markanddavid@gmail.com>
2016-10-22 20:39:42 -0400

    Optimize gif quality by generating a palette

--- convert.rb
+++ convert.rb
@@ -1,3 +1,4 @@
 def convert(video_path, gif_destination_path)
-  `ffmpeg -i "#{video_path}" -pix_fmt rgb24 -r 4 "#{gif_destination_path}"`
+  `ffmpeg -y -i #{video_path} -vf fps=10,palettegen palette.png`
+  `ffmpeg -i "#{video_path}" -i 'palette.png' -filter_complex "fps=10,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse" "#{gif_destination_path}"`
 end
