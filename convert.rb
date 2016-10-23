09a3e46261ee50ff319581eb141b3819f75499b9
Matt and Mark and David <mrothenberg+markanddavid@gmail.com>
2016-10-22 20:43:28 -0400

    Clean up palette after using it.

--- convert.rb
+++ convert.rb
@@ -1,4 +1,12 @@
 def convert(video_path, gif_destination_path)
-  `ffmpeg -y -i #{video_path} -vf fps=10,palettegen palette.png`
-  `ffmpeg -i "#{video_path}" -i 'palette.png' -filter_complex "fps=10,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse" "#{gif_destination_path}"`
+  begin
+
+    `ffmpeg -y -i #{video_path} -vf fps=10,palettegen '/tmp/palette.png'`
+    `ffmpeg -i "#{video_path}" -i '/tmp/palette.png' -filter_complex "fps=10,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse" "#{gif_destination_path}"`
+
+  ensure
+
+    File.delete('/tmp/palette.png') if File.exists? '/tmp/palette.png'
+
+  end
 end
